d0d75bb0351cd0cfdb1c421d699588fa68dc9003
Sriraj <behera.sriraj2911@gmail.com>
2021-08-31 02:44:51 +0530

    implemented basic middleware

--- server/index.js
+++ server/index.js
@@ -3,8 +3,24 @@ const app = express();
 
 const PORT = 5000;
 
+// MiddleWare is something or a piece of code which takes the incoming request and it modifies it before it reaches the route handler
+// when provided no parameters, our server will hang.
+const customMiddleWare = (req, res, next) => {
+    console.log("MiddleWare Executed");
+    next(); // will stop if no furthur middleware or will jump to next middleware if present
+}
+
+// app.use(customMiddleWare); // will run everytime before the GET request (for all requests)
+
 app.get('/', (req, res) => {
+    console.log("This will be printed after the middle ware is executed");
     res.send("Hello World");
+})
+
+// when we pass another parameter then middleware can be exected in that request only
+app.get('/about', customMiddleWare, (req, res) => {
+    console.log("This will be printed after the middle ware is executed");
+    res.send("Hello World About");
 })
 
 app.listen(PORT, () => {
