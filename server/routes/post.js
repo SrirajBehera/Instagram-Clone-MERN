d87d6ba1b0b19d0d0bc471dfa123911b4fdf6e0f
Sriraj <behera.sriraj2911@gmail.com>
2021-11-02 02:25:55 +0530

    completed myposts route

--- server/routes/post.js
+++ server/routes/post.js
@@ -6,13 +6,13 @@ const Post = mongoose.model('Post');
 
 router.get('/allposts', (req, res) => {
   Post.find() // no conditions here so we get all the posts
-  .populate("postedBy", "_id name") // to populate the postedBy field, 2nd parameter is to populate the postedBy conditionally
-  .then((posts) => {
-    res.json({posts})
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+    .populate("postedBy", "_id name") // to populate the postedBy field, 2nd parameter is to populate the postedBy conditionally
+    .then((posts) => {
+      res.json({ posts })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 })
 
 router.post('/createpost', requireLogin, (req, res) => {
@@ -34,6 +34,17 @@ router.post('/createpost', requireLogin, (req, res) => {
   post.save().then((result) => {
     res.json({ post: result })
   })
+    .catch((err) => {
+      console.log(err)
+    })
+})
+
+router.get('/myposts', requireLogin, (req, res) => {
+  Post.find({ postedBy: req.user._id }) // since we are trying to access user details so we need requireLogin middleware
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      res.json({ posts })
+    })
     .catch((err) => {
       console.log(err)
     })
