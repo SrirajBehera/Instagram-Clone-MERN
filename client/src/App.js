1a08c536cd1d5d4e5ad472cfa385f961dfdb525b
Sriraj <behera.sriraj2911@gmail.com>
2021-11-02 03:25:00 +0530

    added react-route, bootstrap and made diff components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/screen/Home';
+import Profile from './components/screen/Profile';
+import SignIn from './components/screen/SignIn';
+import SignUp from './components/screen/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/signin'>
+        <SignIn />
+      </Route>
+      <Route path='/signup'>
+        <SignUp />
+      </Route>
+      <Route path='/profile'>
+        <Profile />
+      </Route>
+    </BrowserRouter>
   );
 }
 
