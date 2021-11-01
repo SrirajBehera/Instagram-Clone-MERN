9c52976f26db0f9a1a1d933b93c4dfb2f0dd6250
Sriraj <behera.sriraj2911@gmail.com>
2021-11-02 04:19:19 +0530

    completed ui of signin and signup screen

--- client/src/components/screen/SignIn.js
+++ client/src/components/screen/SignIn.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>SignIn</h1>
+    <div class="outer-container">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">Sign In</h5>
+          <form>
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+            </div>
+            <div class="mb-3">
+              <label for="exampleInputPassword1" class="form-label">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" />
+            </div>
+            <button type="submit" class="btn btn-primary">SIGN IN</button>
+          </form>
+          <h6 class="mt-3">
+            <Link to="/signup" >Don't have an account?</Link>
+          </h6>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/components/screen/SignUp.js
+++ client/src/components/screen/SignUp.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <div>
-      <h1>SignUp</h1>
+      <div class="outer-container">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Sign Up</h5>
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputName" />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" />
+              </div>
+              <button type="submit" class="btn btn-primary">SIGN UP</button>
+            </form>
+            <h6 class="mt-3">
+            <Link to="/signin" >Already have an account?</Link>
+          </h6>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
