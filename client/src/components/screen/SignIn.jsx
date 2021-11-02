a4a5cd615e769a3ba4901fa9aa628306966ce35b
Sriraj <behera.sriraj2911@gmail.com>
2021-11-03 02:05:57 +0530

    implemented signin API in signin screen

--- client/src/components/screen/SignIn.jsx
+++ client/src/components/screen/SignIn.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 
 const SignIn = () => {
+
+  const history = useHistory()
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const loginUser = () => {
+    fetch("/signin", {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        if (data.error) {
+          console.log(data.error)
+          alert(data.error)
+        } else {
+          alert("Signed In Successfully!")
+          history.push('/')
+        }
+      })
+      .catch(error => console.log(error))
+  }
+
   return (
     <div class="outer-container">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">Sign In</h5>
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" />
-            </div>
-            <button type="submit" class="btn btn-primary">SIGN IN</button>
-          </form>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" value={email} onChange={(res) => setEmail(res.target.value)} />
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" value={password} onChange={(res) => setPassword(res.target.value)} />
+          </div>
+          <button class="btn btn-primary" onClick={() => loginUser()}>SIGN IN</button>
           <h6 class="mt-3">
             <Link to="/signup" >Don't have an account?</Link>
           </h6>
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,77 +12,78 @@ const User = mongoose.model('User');
 const requireLogin = require('../middleware/requireLogin');
 
 router.get('/', (req, res) => {
-    res.send("HELLO");
+  res.send("HELLO");
 });
 
 router.get('/protected', requireLogin, (req, res) => {
-    res.send("Hello User");
+  res.send("Hello User");
 })
 
 router.post('/signup', (req, res) => {
-    console.log(req.body);
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-        // code 422 - server has understood the request but couldn't process the same
-        return res.status(422).json({ error: "Please add all the fields" });
-    }
-    // res.json({ message: "Successfully Posted" });
+  console.log(req.body);
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    // code 422 - server has understood the request but couldn't process the same
+    return res.status(422).json({ error: "Please add all the fields" });
+  }
+  // res.json({ message: "Successfully Posted" });
 
-    User.findOne({ email: email })
-        .then((savedUser) => {
-            if (savedUser) {
-                return res.status(422).json({ error: "User with that email already exists." });
-            }
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (savedUser) {
+        return res.status(422).json({ error: "User with that email already exists." });
+      }
 
-            bcrypt.hash(password, 16)
-                .then((hashedPassword) => {
-                    const user = new User({
-                        name: name, // if key and value are both same then we can condense it to just name, email, etc.
-                        email: email,
-                        password: hashedPassword
-                    });
+      bcrypt.hash(password, 16)
+        .then((hashedPassword) => {
+          const user = new User({
+            name: name, // if key and value are both same then we can condense it to just name, email, etc.
+            email: email,
+            password: hashedPassword
+          });
 
-                    user.save()
-                        .then((user) => {
-                            res.json({ message: "User Created Successfully!" });
-                        })
-                        .catch((err) => {
-                            console.log(`Error saving user - ${err}`);
-                        })
-                });
-        })
-        .catch((err) => {
-            console.log(`Error in email findOne - ${err}`);
+          user.save()
+            .then((user) => {
+              res.json({ message: "User Created Successfully!" });
+            })
+            .catch((err) => {
+              console.log(`Error saving user - ${err}`);
+            })
         });
+    })
+    .catch((err) => {
+      console.log(`Error in email findOne - ${err}`);
+    });
 });
 
 router.post('/signin', (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        res.status(422).json({ error: "Please enter email or password" });
-    }
+  if (!email || !password) {
+    res.status(422).json({ error: "Please enter email or password" });
+  }
 
-    User.findOne({ email: email })
-        .then((savedUser) => {
-            if (!savedUser) {
-                return res.status(422).json({ error: "Invalid email or password" });
-            }
-            bcrypt.compare(password, savedUser.password)
-                .then((doMatch) => { // doMatch is a boolean value
-                    if (doMatch) {
-                        // res.json({ message: "Successfully Signed In" });
-                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
-                        res.json({ token: token });
-                    }
-                    else {
-                        return res.status(422).json({ error: "Invalid email or password" });
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
+  User.findOne({ email: email })
+    .then((savedUser) => {
+      if (!savedUser) {
+        return res.status(422).json({ error: "Invalid email or password" });
+      }
+      bcrypt.compare(password, savedUser.password)
+        .then((doMatch) => { // doMatch is a boolean value
+          if (doMatch) {
+            // res.json({ message: "Successfully Signed In" });
+            const { _id, name, email } = savedUser
+            const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+            res.json({ token: token, user: {_id, name, email} });
+          }
+          else {
+            return res.status(422).json({ error: "Invalid email or password" });
+          }
         })
+        .catch((err) => {
+          console.log(err);
+        })
+    })
 })
 
 module.exports = router;
